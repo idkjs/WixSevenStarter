1efcba256e1f030162af15b3502f4918d6283e02
Alain Armand <aarmand.inbox@gmail.com>
2020-10-18 15:59:37 +0200

    README.md

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { Navigation } from "react-native-navigation";
 import App from './App';
 // import {name as appName} from './app.json';
 
-// AppRegistry.registerComponent('com.myApp.WelcomeScreen', () => App);
+// AppRegistry.registerComponent(appName, () => App);
 Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
 Navigation.events().registerAppLaunchedListener(() => {
    Navigation.setRoot({
